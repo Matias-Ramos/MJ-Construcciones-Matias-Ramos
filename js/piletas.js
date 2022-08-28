e3729ffb0d938f5f62b8a2e903679a627ee72146
Matias <mattsk97@gmail.com>
2022-08-28 18:29:28 -0300

    not really sure what this commit is...

--- js/piletas.js
+++ js/piletas.js
@@ -4,25 +4,29 @@ function expandirForm()
     let formContacto = document.createElement("div");
     formContacto.setAttribute("class","col-10 col-xs-10 col-sm-10 col-lg-6 col-xl-6");
     formContacto.innerHTML = `
-    <form class="mb-4">
+    <form class="my-4">
         <!--Zona-->
         <div class="form-group">
             <label for="zona">Ubicación de la obra: </label>
             <textarea class="form-control" id="zona" rows="1"></textarea>
         </div>
-
         <!--Descripción libre-->
         <div class="form-group">
             <label for="descripcionLibre">Información adicional que desee indicar: </label>
-            <textarea class="form-control" id="descripcionLibre" placeholder="Por ejemplo:&#10;-mts² aproximados.&#10;-Material deseado.&#10;-Otro." rows="4"></textarea>
+            <textarea class="form-control" id="descripcionLibre" placeholder="Por ejemplo:&#10;-Medida: 3x7, 4x8, 5x10.&#10;-Con revestimientos.&#10;-Con vereda térmica.&#10;-Otros." rows="5"></textarea>
         </div>
-
         <!--email-->
         <div class="form-group">
             <label for="email">Direccion de correo electrónico:</label>
-            <input type="email" class="form-control" id="email" placeholder="nombre@gmail.com">
+            <input type="email" class="form-control" id="email">
         </div>
 
+        <!--Teléfono-->
+        <div class="form-group">
+            <label for="telefono">Teléfono:</label>
+            <input class="form-control" id="telefono">
+        </div>
+        
         <button type="submit" class="btn btn-primary mt-3 mb-5 w-50">Enviar</button>
     </form>`;
     divAsesoram.append(formContacto);
@@ -51,32 +55,20 @@ function abrirSeccionAsesoramiento()
 }
 function actualizarStorageTrue()
 {
-    formActivo = sessionStorage.getItem("asesoramientoStatus");
-    formActivo = JSON.parse(formActivo);
-    formActivo.asesoramientoClickeado=true;
-    formActivo = JSON.stringify(formActivo);
-    sessionStorage.setItem('asesoramientoStatus', formActivo);
+    sessionStorage.setItem('asesoramPiletasClicked', true);
     abrirSeccionAsesoramiento();
 }
-function checkStorage()
+function checkStorage(formDisplayed)
 {
-    //(if btnSolicitarAsesoram. nunca clickeado) -> creación status default: "false".
-    if(sessionStorage.getItem("asesoramientoStatus")===null) 
-    {
-        formActivo = JSON.stringify({asesoramientoClickeado : false});
-        sessionStorage.setItem('asesoramientoStatus', formActivo);
-    }
+    if(formDisplayed===null) //(if btnExpandirAsesoram. nunca clickeado) -> creación status default: "false".
+        sessionStorage.setItem('asesoramPiletasClicked', false);
     else
     {
-        formActivo = sessionStorage.getItem("asesoramientoStatus");
-        formActivo = JSON.parse(formActivo); 
-        //Si ya fue clickeado, mantener el formulario abierto.
-        if(formActivo.asesoramientoClickeado==true)
+        if(formDisplayed=="true") //Si ya fue clickeado, mantener el formulario abierto.
             abrirSeccionAsesoramiento();
     }
 }
 
-window.onload = () => checkStorage();
-let formActivo;
+window.onload = () => checkStorage(sessionStorage.getItem("asesoramPiletasClicked"));
 let btnExpandirAsesoram = document.querySelector(".solicitarAsesoram button");
 btnExpandirAsesoram.addEventListener("click",actualizarStorageTrue);
---
+++ js/remodelaciones.js
@@ -0,0 +1,74 @@
+function expandirForm()
+{
+    let divAsesoram = document.querySelector(".solicitarAsesoram section");
+    let formContacto = document.createElement("div");
+    formContacto.setAttribute("class","col-10 col-xs-10 col-sm-10 col-lg-6 col-xl-6");
+    formContacto.innerHTML = `
+    <form class="my-4">
+        <!--Zona-->
+        <div class="form-group">
+            <label for="zona">Ubicación de la obra: </label>
+            <textarea class="form-control" id="zona" rows="1"></textarea>
+        </div>
+        <!--Descripción libre-->
+        <div class="form-group">
+            <label for="descripcionLibre">Información adicional que desee indicar: </label>
+            <textarea class="form-control" id="descripcionLibre" placeholder="Por ejemplo:&#10;-Espacio a remodelar.&#10;-Material deseado.&#10;-Otros." rows="4"></textarea>
+        </div>
+        <!--email-->
+        <div class="form-group">
+            <label for="email">Direccion de correo electrónico:</label>
+            <input type="email" class="form-control" id="email">
+        </div>
+
+        <!--Teléfono-->
+        <div class="form-group">
+            <label for="telefono">Teléfono:</label>
+            <input class="form-control" id="telefono">
+        </div>
+        
+        <button type="submit" class="btn btn-primary mt-3 mb-5 w-50">Enviar</button>
+    </form>`;
+    divAsesoram.append(formContacto);
+
+    let agrandarFuente = () =>
+    {
+    let formContent = document.querySelectorAll(".solicitarAsesoram .form-group");
+    for(let i=0; i<formContent.length; i++)
+        formContent[i].style.fontSize = "larger";
+    }
+    agrandarFuente();
+
+    
+}
+function deshabilitarBtn()
+{
+    btnExpandirAsesoram.setAttribute("disabled","");
+    btnExpandirAsesoram.setAttribute("class","btn btn-outline-primary w-50")
+    btnExpandirAsesoram.style.boxShadow = "none";
+    btnExpandirAsesoram.style.height = "100px";
+}
+function abrirSeccionAsesoramiento()
+{
+    expandirForm();
+    deshabilitarBtn();
+}
+function actualizarStorageTrue()
+{
+    sessionStorage.setItem('asesoramRemodelacionesClicked', true);
+    abrirSeccionAsesoramiento();
+}
+function checkStorage(formDisplayed)
+{
+    if(formDisplayed===null) //(if btnExpandirAsesoram. nunca clickeado) -> creación status default: "false".
+        sessionStorage.setItem('asesoramRemodelacionesClicked', false);
+    else
+    {
+        if(formDisplayed=="true") //Si ya fue clickeado, mantener el formulario abierto.
+            abrirSeccionAsesoramiento();
+    }
+}
+
+window.onload = () => checkStorage(sessionStorage.getItem("asesoramRemodelacionesClicked"));
+let btnExpandirAsesoram = document.querySelector(".solicitarAsesoram button");
+btnExpandirAsesoram.addEventListener("click",actualizarStorageTrue);
